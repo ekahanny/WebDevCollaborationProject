1d4035a17335aec939b3b300a675cb9b3868a840
ekahanny <ekahnny@gmail.com>
2024-03-07 17:24:25 +0800

    menambahkan required untuk setiap kolom input

--- week-3/assets/main.js
+++ week-3/assets/main.js
@@ -1,20 +1,67 @@
 // BUTTON BACK TO TOP
-let toTopButton = document.getElementById('btn-back-to-top')
+let toTopButton = document.getElementById("btn-back-to-top");
 
-window.onscroll = () => scrollFunction()
+window.onscroll = () => scrollFunction();
 
 function scrollFunction() {
-	toTopButton.style.display = document.body.scrollTop > 250 || document.documentElement.scrollTop > 250 ? 'block' : 'none'
+  toTopButton.style.display =
+    document.body.scrollTop > 250 || document.documentElement.scrollTop > 250
+      ? "block"
+      : "none";
 }
 
-toTopButton.addEventListener('click', () => {
-	document.body.scrollTop = 0
-	document.documentElement.scrollTop = 0
-})
+toTopButton.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
 
 // BUTTON SUBMIT
-document.getElementById('form-registrasi').addEventListener('submit', function (event) {
-	event.preventDefault()
+document
+  .getElementById("form-registrasi")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-	window.location.href = 'succes-page.html'
-})
+    window.location.href = "succes-page.html";
+  });
+
+// Required untuk status pernikahan
+const statusPernikahan = document.getElementById("inputStatusPernikahan");
+statusPernikahan.addEventListener("change", function () {
+  if (statusPernikahan.value === "") {
+    statusPernikahan.setAttribute("required", "");
+  } else {
+    statusPernikahan.removeAttribute("required");
+  }
+});
+
+// Validasi Number untuk Tinggi & Berat Badan
+const tinggiBadan = document.getElementById("inputTinggi");
+const beratBadan = document.getElementById("inputBerat");
+const tinggiAlert = document.getElementById("tinggiInvalid");
+const beratAlert = document.getElementById("beratInvalid");
+
+beratBadan.addEventListener("input", () => {
+  if (isNaN(beratBadan.value)) {
+    beratAlert.style.visibility = "visible";
+    beratBadan.removeAttribute("class", "form-control");
+    beratBadan.setAttribute("class", "form-control is-invalid");
+    beratBadan.value = "";
+  } else {
+    beratAlert.style.visibility = "hidden";
+    beratBadan.removeAttribute("class", "form-control is-invalid");
+    beratBadan.setAttribute("class", "form-control");
+  }
+});
+
+tinggiBadan.addEventListener("input", () => {
+  if (isNaN(tinggiBadan.value)) {
+    tinggiAlert.style.visibility = "visible";
+    tinggiBadan.removeAttribute("class", "form-control");
+    tinggiBadan.setAttribute("class", "form-control is-invalid");
+    tinggiBadan.value = "";
+  } else {
+    tinggiAlert.style.visibility = "hidden";
+    tinggiBadan.removeAttribute("class", "form-control is-invalid");
+    tinggiBadan.setAttribute("class", "form-control");
+  }
+});
